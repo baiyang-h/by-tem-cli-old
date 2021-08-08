bdb8eebe04a7c2cf331ceb022459bd11e4a90698
hhq <honghq@weipaitang.com>
2021-08-08 14:32:42 +0800

    mod: 初始化

---
+++ config/template/index.js
@@ -0,0 +1,7 @@
+const reactTemConfig = require('./react');
+const vueTemConfig = require('./vue');
+
+module.exports = {
+  react: reactTemConfig,
+  vue: vueTemConfig,
+}
---
+++ config/template/react.js
@@ -0,0 +1,28 @@
+const basePath = 'direct:http://github.com/visitor-h/r-templates'
+// https://github.com/visitor-h/r-templates/tree/master/Drawer
+module.exports = [
+  {
+    name: 'List',
+    path: `${basePath}/Drawer`,
+  },
+  {
+    name: 'Drawer',
+    path: `${basePath}`,
+  },
+  {
+    name: 'Form',
+    path: `${basePath}`,
+  },
+  {
+    name: 'FormModal',
+    path: `${basePath}`,
+  },
+  {
+    name: 'Modal',
+    path: `${basePath}`,
+  },
+  {
+    name: 'TableModal',
+    path: `${basePath}`,
+  }
+]
